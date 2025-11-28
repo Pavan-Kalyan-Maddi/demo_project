d061a1f115cd6c633cf229f84f7ad049736759fa
pavankalyanmaddi <pavansrinivasvirtue@gmail.com>
2025-11-28 13:40:06 -0600

    new change in code

--- login.java
+++ login.java
@@ -1,4 +1,5 @@
-public clas login{
-public static void main (String[] args]){
-	System.out.print("login";
-)}}
+public class login{
+public static void main (String[] args){
+
+	System.out.print("login");
+}}
